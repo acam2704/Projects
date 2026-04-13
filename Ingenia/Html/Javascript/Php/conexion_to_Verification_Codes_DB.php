8ac8d762faae7d5aba029ca2f4ece628bd89da1f
acam2704 <acam2708@gmail.com>
2026-04-12 21:49:19 -0600

    Update conexion_to_Verification_Codes_DB.php

--- Ingenia/Html/Javascript/Php/conexion_to_Verification_Codes_DB.php
+++ Ingenia/Html/Javascript/Php/conexion_to_Verification_Codes_DB.php
@@ -1,9 +1,12 @@
 <?php
 
-$conexion = new msqli("localhost", "root", "", "ingenia");
+$conexion = new mysqli("localhost", "root", "", "ingenia");
 
 if($conexion->connect_error){
-    die("Error: " . $conexion->connect_error);
+    echo json_encode([
+        'error' => $e,
+        'status' => 'die',
+        'msg' => 'error en conexión',
+    ]);
 }
-
 ?>
