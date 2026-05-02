3687a8938722494349c3811137d25a5deaa14e3b
acam2704 <acam2708@gmail.com>
2026-05-01 22:33:39 -0600

    NO ENTIENDO NADA

--- Ingenia/Html/Php/Code-Verification.php
+++ Ingenia/Html/Php/Code-Verification.php
@@ -14,8 +14,6 @@ try{
     if($_SERVER["REQUEST_METHOD"] === "POST"){
         $params = array($data['email'], $now);
         $stmt = sqlsrv_query($conexion, $sql_request, $params);
-        echo $stmt;
-        echo print_r($stmt);
         if($stmt === false){
             die(json_encode([
                 'status' => 'failed',
