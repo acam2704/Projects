d6db33ffb12a68265fcd058a29c517a7c1384ad1
acam2704 <acam2708@gmail.com>
2026-05-01 22:05:34 -0600

    array corrected at show_codes.php

--- Ingenia/Html/Php/Code-Verification.php
+++ Ingenia/Html/Php/Code-Verification.php
@@ -12,10 +12,7 @@ $sql_request = "SELECT TOP 1 * FROM verification_codes WHERE email = ? AND expir
 
 try{
     if($_SERVER["REQUEST_METHOD"] === "POST"){
-        $params = [
-            $data['email'],
-            $now
-        ];
+        $params = array($data['email'], $now);
         $stmt = sqlsrv_query($conexion, $sql_request, $params);
         if($stmt === false){
             die(json_encode([
