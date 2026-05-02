5d862bdbbf950c46d4d5ca4a0ad2bce03380df31
acam2704 <acam2708@gmail.com>
2026-05-01 18:47:26 -0600

    Modified of the view at show_codes.php

--- Ingenia/Html/Php/show_codes.php
+++ Ingenia/Html/Php/show_codes.php
@@ -30,8 +30,8 @@ if($stmt === false){
 $unique_result = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
 
 while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-    print_r($row['code'] . '<> ');
-    print_r($row[0] . $row[1] . $row[2] . '<> ');
+    echo $row['code'] . '<> ';
+    echo $row[0] . $row[1] . $row[2] . '<> ';
     print_r($row . '<br>');
 }
 //if(password_verify($data['code'], $unique_result[0])){
