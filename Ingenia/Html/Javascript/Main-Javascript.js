b8444016accc8842d497b6b7326f5458ebf50366
acam2704 <acam2708@gmail.com>
2026-05-04 20:31:47 -0600

    new code at go_back in js

--- Ingenia/Html/Javascript/Main-Javascript.js
+++ Ingenia/Html/Javascript/Main-Javascript.js
@@ -170,13 +170,18 @@ function Go_back(){
     // Se reestablece el marginTop de bttn_send
     bttn_send.style.marginTop = "20px";
     
-    if(getComputedStyle(input_name_Re).display !== "none"){
-
-    } else if(getComputedStyle(input_code_Re).display !== "none"){
-        // No se hace nada
-    } else{
-        able_inputs(personal_information);
-        hide_and_show(personal_information_container, security_information);
+    if(getComputedStyle(verification_code_container).display !== "none"){
+        able_inputs(personal_information_container);
+        hide_and_show(personal_information_container, verification_code_container);
+    } else if(getComputedStyle(identity_information_container).display !== "none"){
+        able_inputs(personal_information_container);
+        hide_and_show(personal_information_container, identity_information_container);
+    } else if(getComputedStyle(security_information_container).display !== "none"){
+        able_inputs(identity_information_container);
+        hide_and_show(identity_information_container, security_information_container);
+    } else if(getComputedStyle(public_profile_information_container).display !== "none"){
+        able_inputs(security_information_container);
+        hide_and_show(security_information_container, public_profile_information_container);
     }
 }
 
