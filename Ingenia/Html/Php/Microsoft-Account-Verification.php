0af4dc71ff96a502649a8cdfb831a40f0ec93c8c
acam2704 <acam2708@gmail.com>
2026-05-06 17:06:04 -0600

    corrections

--- Ingenia/Html/Php/Microsoft-Account-Verification.php
+++ Ingenia/Html/Php/Microsoft-Account-Verification.php
@@ -68,6 +68,7 @@ try{
 <script>
     // Guardar datos en el navegador
     sessionStorage.setItem('user', '<?php echo $respond; ?>');
+    localStorage.setItem('user', '<?php echo $respond ?>');
 
     // Redirigir al HTML
     window.location.href = "https://ingenia-a6dkhcarh6e3b0ak.mexicocentral-01.azurewebsites.net/Ingenia/Html/Session-Log.html";
