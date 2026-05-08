7a438ed7ea312bf029ad3e6c873bfee15335a49c
acam2704 <acam2708@gmail.com>
2026-05-07 22:56:37 -0600

    End code at upload_picture.php

--- Ingenia/Html/Php/upload_picture.php
+++ Ingenia/Html/Php/upload_picture.php
@@ -4,23 +4,15 @@ header('Access-Control-Allow-Methods: OPTIONS, POST, GET');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $picture = $_FILES['imagen'];
-    echo $archivo['name'];
     $extension = pathinfo($archivo['name'], PATHINFO_EXTENSION);
-    echo $extension;
 
     $safeName = uniqid() . '.' . $extension;
-    echo $safeName;
     $ruta = 'uploads/' . $safeName;
-    echo $ruta;
-
-    echo $archivo['tmp_name'];
-
-    echo $archivo;
-    /* move_uploaded_file(
+    move_uploaded_file(
         $archivo['tmp_name'],
         $ruta
     );
 
-    echo $ruta; */
+    echo $ruta;
 }
 ?>
