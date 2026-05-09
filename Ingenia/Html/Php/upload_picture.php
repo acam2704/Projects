16cf64d50968ef8b00265e450ba0e2c7b1d691b6
acam2704 <acam2708@gmail.com>
2026-05-09 14:00:09 -0600

    echo in php

--- Ingenia/Html/Php/upload_picture.php
+++ Ingenia/Html/Php/upload_picture.php
@@ -5,12 +5,14 @@ header('Access-Control-Allow-Methods: OPTIONS, POST, GET');
 require_once '/home/site/wwwroot/Ingenia/Html/vendor/autoload.php';
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+echo json_encode([
+    'status' => 'on process',
+    'error' => null,
+    'data' => null
+]);
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     try {
-        file_put_contents("/home/site/wwwroot/Ingenia/Html/DEBUG_ACTIVE.log",
-        "NEW VERSION RUNNING " . date('c') . "\n",
-        FILE_APPEND);
         if(!isset($_FILES['picture'])){throw new Error('No se envió ningún archivo');}
 
         $received_file = $_FILES['picture'];
@@ -28,6 +30,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $fileName = bin2hex(random_bytes(16)) . '.' . $extention;
         $connectionString = getenv('blobStorage_connectionString_1');
         $containerName = getenv('container1');
+        echo json_encode([
+            'status' => 'on process',
+            'extention' => $extention,
+            'fileName' => $fileName,
+            'error' => null
+        ]);
         $blobClient = BlobRestProxy::createBlobService($connectionString);
         $content = file_get_contents($received_file['tmp_name']);
         $blobClient->createBlockBlob(
