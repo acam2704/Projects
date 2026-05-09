1a201f85b101fbae245471ff408ed874a15c4ba8
acam2704 <acam2708@gmail.com>
2026-05-09 15:18:02 -0600

    New code html and js

---
+++ Ingenia/Html/Php/conexion_learning_machine.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+
+$json_data = file_get_contents('php://input');
+$data = json_decode($json_data, true);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    echo '1';
+    $output = shell_exec("python3 python/learning_machine.py '" . json_encode($data) . "'");
+    
+    if(!$output){
+        echo json_encode([
+        'status' => 'ok',
+        'error' => null,
+        'msg' => 'Python successfully ran', 
+        'data' => $output
+        ]);
+    }
+
+} else {
+    echo json_encode([
+        'status' => 'failed',
+        'error' => 'Invalid request method',
+        'msg' => 'Only POST admitted',
+        'data' => null
+    ])
+}
+?>
---
+++ Ingenia/Html/Php/validación_reseña.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+
+include('conexion_learning_machine.php')
+
+$json_data = file_get_contents('php://input');
+$data = json_decode($jason_data, true);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $output = shell_exec("python3 script.py '" . $json . "'");
+    echo json_encode([
+        'status' => 'ok',
+        'error' => null,
+        'msg' => 'Python successfully ran', 
+        'data' => $output
+        ]);
+} else {
+    echo json_encode([
+        'status' => 'failed',
+        'error' => 'Invalid request method',
+        'msg' => 'Only POST admitted',
+        'data' => null
+    ])
+}
+
+?>
