965b8cc0a0267bdc6b3e4335cf48ad21437805c9
acam2704 <acam2708@gmail.com>
2026-05-14 13:20:44 -0600

    Update Main-Javascript.js

--- Ingenia/Html/Javascript/Main-Javascript.js
+++ Ingenia/Html/Javascript/Main-Javascript.js
@@ -335,9 +335,12 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('inputs_container').querySelectorAll(':scope > article').forEach(article => {
     article.querySelectorAll(':scope > input, select').forEach(element => {
         let span = element.previousElementSibling;
+        console.log('0' + span);
         while(!(span instanceof HTMLSpanElement)){
             span = span.previousElementSibling;
+            console.log('1' + span);
             if(!span){span = element.previousElementSibling; span = span.parentElement.previousElementSibling;}
+            console.log('2' + span);
         }
         element.addEventListener('change', function() {
             span.style.display = 'none';
