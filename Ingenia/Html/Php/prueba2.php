4167b67117e00634fb94716ade967f190042c0b1
acam2704 <acam2708@gmail.com>
2026-05-21 12:13:43 -0600

    Update prueba2.php

--- Ingenia/Html/Php/prueba2.php
+++ Ingenia/Html/Php/prueba2.php
@@ -1,10 +1,16 @@
 <?php
-    $url = "http://169.254.169.254/metadata/instance?api-version=2021-02-01";
+$url = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://cognitiveservices.azure.com";
 
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Metadata: true"]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$ch = curl_init($url);
 
-    echo curl_exec($ch);
-    echo 'hola';
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Metadata: true"
+]);
+
+$response = curl_exec($ch);
+
+var_dump($response);
+var_dump(curl_errno($ch));
+var_dump(curl_error($ch));
 ?>
