c9c26d5275c05369a687d8de463c02acfeabfa05
acam2704 <acam2708@gmail.com>
2026-05-21 12:37:03 -0600

    Update prueba2.php

--- Ingenia/Html/Php/prueba2.php
+++ Ingenia/Html/Php/prueba2.php
@@ -1,5 +1,8 @@
 <?php
-$url = "http://169.254.169.254/metadata/instance?api-version=2021-02-01";
+$endpoint = getenv("IDENTITY_ENDPOINT");
+$secret = getenv("IDENTITY_HEADER");
+$resource = urlencode("https://cognitiveservices.azure.com");
+$url = $endpoint . "?resource=$resource&api-version=2019-08-01";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
