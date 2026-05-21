b0a0b5ddb05549d952cf118198b2e5822836c3a2
acam2704 <acam2708@gmail.com>
2026-05-21 12:35:22 -0600

    Update scan_img.php

--- Ingenia/Html/Php/scan_img.php
+++ Ingenia/Html/Php/scan_img.php
@@ -5,8 +5,11 @@ header("Access-Control-Allow-Methods: GET, OPTIONS, POST");
 header('Content-Type: application/json');
 
 try{
-    $resource = 'https://cognitiveservices.azure.com';
-    $url = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=" . urlencode($resource);
+    $endpoint = getenv("IDENTITY_ENDPOINT");
+    $secret = getenv("IDENTITY_HEADER");
+    $resource = urlencode("https://cognitiveservices.azure.com");
+    $url = $endpoint . "?resource=$resource&api-version=2019-08-01";
+
 
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [ "Metadata: true" ]);
