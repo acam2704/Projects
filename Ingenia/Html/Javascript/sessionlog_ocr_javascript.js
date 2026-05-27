0daa30e225f14fc7f7557d1a34d2e47b530460e9
Emy <acam2708@gmail.com>
2026-05-27 14:14:35 -0600

    Js

--- Ingenia/Html/Javascript/sessionlog_ocr_javascript.js
+++ Ingenia/Html/Javascript/sessionlog_ocr_javascript.js
@@ -30,6 +30,31 @@ document.getElementById('input_frontdui_OCR').addEventListener('change', async f
     + data.email + '  -  ' + data.firstName + '  -  ' + data.lastName + '\n' + data.phoneNumber + '  -  ' + data.status;
 });
 
+document.getElementById('input_backdui_OCR').addEventListener('change', async function(){
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader(); // Crea el lector de archivos
+        const img = document.getElementById('file_backdui_OCR');
+        reader.onload = function(e) {
+            img.src = e.target.result; // Asigna la ruta de la imagen
+        }
+        reader.readAsDataURL(file); // Lee el archivo como URL de datos
+    }
+
+    const text = document.getElementById('main_alert');
+    const form = new FormData();
+    form.append('file', file);
+
+    const response = await fetch('Php/scan_img.php', {
+        method: 'POST',
+        body: form
+    });
+    const data = await response.json();
+    console.log(data);
+    text.textContent = data.currentAddress + '  -  ' + data.dateOfBirth + '  -  ' + data.documentNumber + '\n'
+    + data.email + '  -  ' + data.firstName + '  -  ' + data.lastName + '\n' + data.phoneNumber + '  -  ' + data.status;
+})
+
 document.getElementById('bttn_frontdui').addEventListener('click', function() {
     const input_file = document.getElementById('input_frontdui_OCR');
     input_file.click();
@@ -39,3 +64,9 @@ document.getElementById('bttn_backdui').addEventListener('click', function() {
     const input_file = document.getElementById('input_backdui_OCR');
     input_file.click();
 });
+
+function validate_dui_info(data, bool){
+    for(const key of Object.keys(data)){
+        console.log('key: ' + key + ' - ' + 'value: ' + data[key]);
+    }
+}
