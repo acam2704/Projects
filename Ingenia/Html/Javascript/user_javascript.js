4f2657b16e821ad12accd1cb9ec46e816cffed49
SamPixel23 <steven.vasquez2026@frech.superate.org>
2026-06-23 07:20:54 -0600

    Update user_javascript.js

--- Ingenia/Html/Javascript/user_javascript.js
+++ Ingenia/Html/Javascript/user_javascript.js
@@ -9,6 +9,17 @@ document.getElementById('user_backbttn').addEventListener('click', () => {
 
     window.location.href = 'https://ingenia-a6dkhcarh6e3b0ak.mexicocentral-01.azurewebsites.net/Ingenia/Html/web.html';
 });
+document.getElementById('construction_worker_bttn').addEventListener('click', () => {
+    let user_data = localStorage.getItem('user');
+    if(!user_data){
+        let json = {rol: 'construction_worker'};
+        localStorage.setItem('user', JSON.stringify(json));
+    }
+    else{user_data['rol'] = 'client';}
+    localStorage.setItem('user', JSON.stringify(user_data));
+
+    window.location.href = 'https://ingenia-a6dkhcarh6e3b0ak.mexicocentral-01.azurewebsites.net/Ingenia/Html/web.html';
+});
 const window_location = window.location['pathname'].split('/');
 const window_pathname = window_location[window_location.length - 1];
 const bttns = ['client_bttn', 'construction_worker_bttn'];
